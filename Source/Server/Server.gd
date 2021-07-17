6a7ffb0ac7d4ca983088b180bca1131cbec7e240
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2021-07-17 08:11:07 +0500

    create_lobby method

--- Source/Server/Server.gd
+++ Source/Server/Server.gd
@@ -1,10 +1,20 @@
 extends Node
 
 const SERVER_PORT = 1909
-const MAX_PLAYERS = 500
+const MAX_PLAYERS = 2000
 
 var players_online = []
-var players_data = {}
+var players_names = {}
+var lobbies = {}
+
+var colors = {
+	0: Color.orange,
+	1: Color.lightblue,
+	2: Color.yellow,
+	3: Color.lightsalmon,
+	4: Color.lightgreen,
+	5: Color.lightcoral
+}
 
 func _ready():
 	connect_signals()
@@ -29,6 +39,20 @@ func _player_connected(player_id):
 func _player_disconnected(player_id):
 	pass
 
-remote func get_player_data(player_data):
+remote func get_player_data(player_name):
 	var player_id = get_tree().get_rpc_sender_id()
-	players_data[player_id] = player_data
+	players_names[player_id] = player_name
+
+remote func create_lobby(lobby_data):
+	# Lobby Code [ID]
+	lobby_data["code"] = lobbies.size()
+	
+	# Setting players data for lobby
+	var player_id = get_tree().get_rpc_sender_id()
+	var player_number = lobby_data["players"].size()
+	lobby_data["players"][player_number]["id"] = player_id
+	lobby_data["players"][player_number]["name"] = players_names[player_id]
+	lobby_data["players"][player_number]["color"] = colors[player_number]
+	
+	# Append lobbies list
+	lobbies[lobbies.size()] = lobby_data
