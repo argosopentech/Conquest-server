d4df63669504e8229f53f90dd497b9c034200676
Wamiq Ur Rehman <wamiqurrehman093@gmail.com>
2023-02-15 22:03:35 +0500

    updates to the high level server file

--- Source/Server/HighLevelServer.gd
+++ Source/Server/HighLevelServer.gd
@@ -1,5 +1,5 @@
 extends Node
-class_name HLServer
+class_name HighLevelServer
 
 var server = NetworkedMultiplayerENet.new()
 var request_handler = null
@@ -7,21 +7,12 @@ var request_handler = null
 signal user_connected(user_id)
 signal user_disconnected(user_id)
 
-func connect_server_signals():
-	if get_tree().is_connected("network_peer_connected", self, "client_connected"):
-		return
-	get_tree().connect("network_peer_connected", self, "client_connected")
-	get_tree().connect("network_peer_disconnected", self, "client_disconnected")
-
-func disconnect_server_signals():
-	if !get_tree().is_connected("network_peer_connected", self, "client_connected"):
-		return
-	get_tree().disconnect("network_peer_connected", self, "client_connected")
-	get_tree().disconnect("network_peer_disconnected", self, "client_disconnected")
+func set_request_handler(new_handler):
+	request_handler = new_handler
 
 func start_server(port = 1909, max_players=2000):
-	connect_server_signals()
 	if server.create_server(port, max_players) == OK:
+		connect_server_signals()
 		print("The server has started.")
 		get_tree().network_peer = server
 		return true
@@ -29,13 +20,11 @@ func start_server(port = 1909, max_players=2000):
 		print("Unable to start the server.")
 		return false
 
-func stop_server():
-	disconnect_server_signals()
-	server.close_connection()
-	get_tree().network_peer = null
-
-func set_request_handler(new_handler):
-	request_handler = new_handler
+func connect_server_signals():
+	if get_tree().is_connected("network_peer_connected", self, "client_connected"):
+		return
+	get_tree().connect("network_peer_connected", self, "client_connected")
+	get_tree().connect("network_peer_disconnected", self, "client_disconnected")
 
 func client_connected(id):
 	print("Client %d has just connected." % [id])
@@ -45,14 +34,14 @@ func client_disconnected(id):
 	print("Client %d has just disconnected." % [id])
 	emit_signal("user_disconnected", id)
 
-remote func received_data_from_client(method_info):
-	var id = get_tree().get_rpc_sender_id()
-	print("Just received a packet from client %d: %s." % [id, method_info])
-	process_method_info(id, method_info)
-
 func send_data_to_client(id, method, data=null):
 	var method_info = {"purpose": "request", "method": method, "data": data}
 	rpc_id(id, "received_data_from_server", method_info)
+
+remote func received_data_from_client(method_info):
+	var id = get_tree().get_rpc_sender_id()
+	print("Just received data from client %d: %s." % [id, method_info])
+	process_method_info(id, method_info)
 
 func process_method_info(id, method_info):
 	if !request_handler: return
@@ -60,3 +49,14 @@ func process_method_info(id, method_info):
 	if method_info is Dictionary & method_info["purpose"] == "request":
 		if request_handler.has_method(method_info["method"]):
 			request_handler.call_deferred(method_info["method"], id, method_info["data"])
+
+func stop_server():
+	disconnect_server_signals()
+	server.close_connection()
+	get_tree().network_peer = null
+
+func disconnect_server_signals():
+	if !get_tree().is_connected("network_peer_connected", self, "client_connected"):
+		return
+	get_tree().disconnect("network_peer_connected", self, "client_connected")
+	get_tree().disconnect("network_peer_disconnected", self, "client_disconnected")
